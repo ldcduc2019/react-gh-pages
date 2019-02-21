5cda394708466e3c626913e606c1890a225e4df1
duc.ldc <duc.ldc@teko.vn>
2019-02-21 14:23:35 +0700

    Updates

---
+++ precache-manifest.095e8607a0fb3c27ed1b35e7b1871c8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-gh-pages/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cde22863ffca018955eb",
+    "url": "/react-gh-pages/static/js/runtime~main.cde22863.js"
+  },
+  {
+    "revision": "b45f1d56295afffcdd64",
+    "url": "/react-gh-pages/static/js/main.b45f1d56.chunk.js"
+  },
+  {
+    "revision": "ba1ca5ba3f9c1e3c91ad",
+    "url": "/react-gh-pages/static/js/2.ba1ca5ba.chunk.js"
+  },
+  {
+    "revision": "b45f1d56295afffcdd64",
+    "url": "/react-gh-pages/static/css/main.fa2773c0.chunk.css"
+  },
+  {
+    "revision": "f9442fb9d220f034c3eed9703f236e2d",
+    "url": "/react-gh-pages/index.html"
+  }
+];
