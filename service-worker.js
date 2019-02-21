50b0c68b4026de80269737234cb3a7f8e858c751
duc.ldc <duc.ldc@teko.vn>
2019-02-21 17:07:08 +0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.d87963c014473e51ac8badf0410f8d3b.js"
+  "/react-gh-pages/precache-manifest.33fe847397a66c108d4cb9228ab494aa.js"
 );
 
 workbox.clientsClaim();
